74f328b1495889784d10451b4c4080a9500a6f1c
Pete <petra.lutsenko@mail.ru>
2020-01-06 18:30:07 +0700

    keyboard work

--- lessons/ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
+++ lessons/ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
@@ -1,8 +1,38 @@
 ﻿#include <iostream>
 #include <freeglut.h>
+#include <stdlib.h>
 
+
+// цвет
+float red = 1.0f, blue = 1.0f, green = 1.0f;
+
+// угол
 float angle = 0.0f;
 
+void processNormalKeys(unsigned char key, int x, int y) {
+    if (key == 27)
+        exit(0);
+}
+
+void processSpecialKeys(int key, int x, int y) {
+    switch (key) {
+    case GLUT_KEY_F1:
+        red = 1.0;
+        green = 0.0;
+        blue = 0.0; 
+        break;
+    case GLUT_KEY_F2:
+        red = 0.0;
+        green = 1.0;
+        blue = 0.0; 
+        break;
+    case GLUT_KEY_F3:
+        red = 0.0;
+        green = 0.0;
+        blue = 1.0; 
+        break;
+    }
+}
 void renderScene(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -12,9 +42,12 @@ void renderScene(void)
     gluLookAt(0.0f, 0.0f, 4.0f,
         0.0f, 0.0f, 0.0f,
         0.0f, 1.0f, 0.0f);
-
+    
+    //поворот на заданную величину
     glRotatef(angle, 0.0f, 1.0f, 0.0f);
 
+    // установить цвет модели
+    glColor3f(red, green, blue);
     glBegin(GL_TRIANGLES);
       glVertex3f(-0.5, -0.5, 0.0);
       glVertex3f(0.0, 0.5, 0.0);
@@ -63,9 +96,11 @@ int main(int argc, char** argv)
     // регистрация вызовов функций GLUT
     glutDisplayFunc(renderScene);
     glutReshapeFunc(changeSize);
-
-    // Новая функция 
     glutIdleFunc(renderScene);
+
+    // Новые функции
+    glutKeyboardFunc(processNormalKeys);
+    glutSpecialFunc(processSpecialKeys);
 
     // цикл обработки событий
     glutMainLoop();
