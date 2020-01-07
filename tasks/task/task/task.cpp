d6d172093337eaaa574a6156b45b426defc94efb
Pete <petra.lutsenko@mail.ru>
2020-01-07 20:07:07 +0700

    construction of an epicycloid and its shift along the coordinate axis

--- tasks/task/task/task.cpp
+++ tasks/task/task/task.cpp
@@ -1,17 +1,55 @@
 ﻿#include <freeglut.h>
 #include <iostream>
 
-float x = 1.5;
-// флаг для движения туда-сюда
-bool flag = true;
+using namespace std;
 
-void changeSize(int w, int h) {
-	// предупредим деление на ноль
-	// если окно сильно перетянуто будет
-	if (h == 0)
-		h = 1;
-	float ratio = 1.0 * w / h;
+// размеры координатной сетки
+float mx = 10.0;
+float my = 10.0;
 
+float dx = 0.0;
+float dy = 0.0;
+
+void processNormalKeys(unsigned char key, int x, int y) {
+	switch (key)
+	{
+	case 27:
+		exit(0);
+		break;
+	case 109:
+		mx += 1.0;
+		my += 1.0;
+		break;
+	case 110:
+		mx -= 1.0;
+		my -= 1.0;
+		break;
+	}
+	glutPostRedisplay();
+}
+
+void processSpecialKeys(int key, int x, int y) {
+	switch (key)
+	{
+	case GLUT_KEY_LEFT:
+		dx -= 1.0;
+		break;
+	case GLUT_KEY_RIGHT:
+		dx += 1.0;
+		break;
+	case GLUT_KEY_UP:
+		dy += 1.0;
+		break;
+	case GLUT_KEY_DOWN:
+		dy -= 1.0;
+		break;
+	}
+	//обновить картинку
+	glutPostRedisplay();
+}
+
+void changeSize(int w, int h) 
+{
 	// используем матрицу проекции
 	glMatrixMode(GL_PROJECTION);
 
@@ -19,58 +57,58 @@ void changeSize(int w, int h) {
 	glLoadIdentity();
 
 	// определяем окно просмотра
-	glViewport(0, 0, w, h);
+	glViewport(0, 0, w + mx, h + my);
 
-	glOrtho(-5.0, 5.0, -5.0, 5.0, 0.0, 10.0);
-	gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
-	// установить корректную перспективу.
-	gluPerspective(45, ratio, 0.1, 100);
+	gluOrtho2D(-mx, mx, -my, my);
+	//gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
 
 	// вернуться к модели
 	glMatrixMode(GL_MODELVIEW);
+	
 }
 
-void renderScene(void) {
+// риусем объект
+void drawObject()
+{
+	// угол
+	float t;
+	// шаг
+	float step = 0.01;
+	glBegin(GL_LINES);
+	glColor3d(0.5, 0.6, 0.2);
+	for (float i = 0; i <= 360.0; i += step)
+	{
+		t = 20 * 3.1415 * i / 180.0;
+		glVertex2d(6.2*(cos(t)-(cos(3.1*t)/3.1)), 6.2 * (sin(t) - (sin(3.1 * t) / 3.1)));
+	}
+	glEnd();
+}
+
+void renderScene(void) 
+{
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 
-	glPushMatrix();
-	glRotatef(45, 0.0, 0.0, 1.0);
-	glColor3f(0.3, 0.5, 0.4);
-	glBegin(GL_TRIANGLES);
-		glVertex3f(x + 1.0, 2.5, -3.0);
-		glVertex3f(x + 0.5, 3.5, -3.0);
-		glVertex3f(x, 2.5, -3.0);
+	glLineWidth(1);       // ширина сетки
+
+    glBegin(GL_LINES); //сетка
+		glColor3d(1, 1, 1);
+		glVertex2d(-mx, 0);
+		glVertex2d(mx, 0);
+		glVertex2d(0, -my);
+		glVertex2d(0, my);
 	glEnd();
 
-	glPopMatrix();
 	glPushMatrix();
-	//glRotatef(-45, 0.0, 0.0, 1.0);
-	glColor3f(0.4, 0.55, 0.2);
-	glBegin(GL_TRIANGLES);
-		glVertex3f(x + 1.0, 2.5, -3.0);
-		glVertex3f(x + 0.5, 3.5, -3.0);
-		glVertex3f(x, 2.5, -3.0);
-	glEnd();
+	glTranslatef(dx, dy, 0.0);
+	drawObject();
 	glPopMatrix();
 
-	if (flag == true)
-	{
-		x += 0.1;
-		if(x >= 3)
-			flag = false;
-	}
-	else
-	{
-		x -= 0.1;
-		if (x <= 1.5)
-			flag = true;
-	}
-	
 	glutSwapBuffers();
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
 
 	// инициализация
 	glutInit(&argc, argv);
@@ -82,7 +120,9 @@ int main(int argc, char** argv) {
 	// регистрация обратных вызовов
 	glutDisplayFunc(renderScene);
 	glutReshapeFunc(changeSize);
-	glutIdleFunc(renderScene);
+	glutKeyboardFunc(processNormalKeys);
+	glutSpecialFunc(processSpecialKeys);
+	glEnable(GL_DEPTH_TEST);
 	// Основной цикл GLUT
 	glutMainLoop();
 
