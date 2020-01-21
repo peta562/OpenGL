4880d635002b7fb0993544029c3a1232aa731b59
Pete <petra.lutsenko@mail.ru>
2020-01-21 23:00:01 +0700

    B-spline

--- tasks/task/task/task.cpp
+++ tasks/task/task/task.cpp
@@ -46,6 +46,7 @@ void changeSize(int w, int h)
 	glMatrixMode(GL_MODELVIEW);
 	
 }
+
 // риусем линию алгоритмом Брезенхема
 void drawLine(int x1, int x2, int y1, int y2)
 {
@@ -78,6 +79,7 @@ void drawLine(int x1, int x2, int y1, int y2)
 	}
 
 }
+
 // риусем окружность алгоритмом Брезенхема
 void drawCircle(int x0, int y0, int radius) {
 	int x = 0;
@@ -108,6 +110,7 @@ void drawCircle(int x0, int y0, int radius) {
 		y--;
 	}
 }
+
 // рисуем прямоугольник
 void drawRectangle(int x1, int x2, int y1, int y2)
 {
@@ -116,6 +119,55 @@ void drawRectangle(int x1, int x2, int y1, int y2)
 	drawLine(x2, x2, y1, y2);
 	drawLine(x1, x2, y2, y2);
 }
+
+float countSplineCoefficient(int index, int x1, int x2, int x3, int x4)
+{
+	switch (index)
+	{
+	case 0:
+		return (x1 + 4 * x2 + x3) / 6.0f; //свободный член
+	case 1:
+		return (-x1 + x3) / 2.0f; //коэффициент при t
+	case 2:
+		return (x1 - 2 * x2 + x3) / 2.0f; //коэффициент при t^2
+	case 3:
+		return (-x1 + 3 * x2 - 3 * x3 + x4) / 6.0f; //коэффициент при t^3
+	}
+
+	return 0;
+}
+
+void drawSpline(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
+{
+	//подсчет коэффициентов сплайна
+	float a0 = countSplineCoefficient(0, x1, x2, x3, x4);
+	float a1 = countSplineCoefficient(1, x1, x2, x3, x4);
+	float a2 = countSplineCoefficient(2, x1, x2, x3, x4);
+	float a3 = countSplineCoefficient(3, x1, x2, x3, x4);
+
+	float b0 = countSplineCoefficient(0, y1, y2, y3, y4);
+	float b1 = countSplineCoefficient(1, y1, y2, y3, y4);
+	float b2 = countSplineCoefficient(2, y1, y2, y3, y4);
+	float b3 = countSplineCoefficient(3, y1, y2, y3, y4);
+
+	float xPrev = a0, yPrev = b0;
+
+	for (int i = 1; i <= 10; i++) //разбиваем кривую на 10 отрезков
+	{
+		float t = i / 10.0f;
+		float x = ((a3 * t + a2) * t + a1) * t + a0; //x(t)
+		float y = ((b3 * t + b2) * t + b1) * t + b0; //y(t)
+
+		glBegin(GL_LINES); 
+			glVertex2f(xPrev, yPrev); 
+			glVertex2f(x, y);
+		glEnd();//соединяем подсчитанные точки
+
+		xPrev = x;
+		yPrev = y;
+	}
+}
+
 // рисуем треугольник
 void drawTriangle(int x1, int x2, int x3, int y1, int y2, int y3)
 {
@@ -140,8 +192,9 @@ void renderScene(void)
 	glTranslatef(dx, dy, 0.0);
 	drawLine(10, 40, 10, 20);
 	drawCircle(-25, -40, 30);
-	drawRectangle(-20, -40, 30, 30);
+	drawRectangle(-20, -40, 30, 50);
 	drawTriangle(23, 33, 38, -10, -10, -30);
+	drawSpline(60, 70, 65, 63, 75, -34, -54, 62);
 	glPopMatrix();
 	glutSwapBuffers();
 }
